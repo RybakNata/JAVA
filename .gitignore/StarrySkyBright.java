6d856ad12d4b018a33616344bf863f8936f42fbc
RybakNata <47082002+RybakNata@users.noreply.github.com>
2019-02-18 18:30:24 +0200

    Зоряне небо

---
+++ .gitignore/StarrySkyBright.java
@@ -0,0 +1,10 @@
+# JAVA
+Home work
+import GrSkyBrightLibrary.GrSkyBrightLibrary;
+
+public class StarrySkyBright {
+
+    public static void main(String[] args) {
+        new GrSkyBrightLibrary();
+    }
+}
